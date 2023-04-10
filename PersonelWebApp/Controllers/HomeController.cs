2100bc9038d80012d9fb8aa694a093c9953b06bc
melike acuner <melikeacuner@gmail.com>
2023-04-10 18:19:08 +0300

    Add project files.

---
+++ PersonelWebApp/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonelWebApp.Models;
+using System.Diagnostics;
+
+namespace PersonelWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult LoginPage()
+        {
+            return PartialView();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Profilim()
+        {
+            return View();
+        }
+
+        public IActionResult Izinler()
+        {
+            return View();
+        }
+
+        public IActionResult Maas()
+        {
+            return View();
+        }
+
+        public IActionResult Takvim()
+        {
+            return View();
+        }
+
+        public IActionResult Duyurular()
+        {
+            return View();
+        }
+
+        public IActionResult Aktiviteler()
+        {
+            return View();
+        }
+
+        public IActionResult Calisanlar()
+        {
+            return View();
+        }
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
