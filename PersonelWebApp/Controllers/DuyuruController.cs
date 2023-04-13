5b473ac6fbfd6a98c4b7f477f2d9d5ee9bea0359
melike acuner <melikeacuner@gmail.com>
2023-04-13 13:40:23 +0300

    controller added

---
+++ PersonelWebApp/Controllers/DuyuruController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonelWebApp.Controllers
+{
+    public class DuyuruController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
