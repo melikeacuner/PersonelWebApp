7d093289a2487d0a1c40d7df61660b4952fed6e2
melike acuner <melikeacuner@gmail.com>
2023-04-13 13:36:34 +0300

    login commited

---
+++ PersonelWebApp/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonelWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
